a443503b0c953e1ee3e6c378600ebaa449edb8ed
Kawthar <kawthar.goumaidi@gmail.com>
2021-10-01 13:45:02 +0200

    Creation de la classe Outline et initialisation

--- tp3-2020--tp3_l2mi_kawthar-master/src/main/java/Outline.java
+++ tp3-2020--tp3_l2mi_kawthar-master/src/main/java/Outline.java
@@ -1,2 +1,13 @@
-public class Outline {
+public class Outline implements Transform{
+
+    private final double threshold;
+
+    public Outline(double threshold) {
+        this.threshold = threshold;
+    }
+
+    @Override
+    public void applyTo(GrayImage image) {
+
+    }
 }
